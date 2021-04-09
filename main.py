a5b33a3da6f355ddc349965695b7ee85f5c97aea
Carson Hamel <carsonhamel@ufl.edu>
2021-04-08 23:08:42 -0400

    pipeline/imports

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
 
 if __name__ == '__main__':
+    count_vect = CountVectorizer()
+    x_train_counts = count_vect.fit()
     print("Ay que fabuloso")
